45424bc7d18b59db625f0fa7b1559bac4d8e565b
koulika <158286470+koulika@users.noreply.github.com>
2024-02-19 12:52:42 +0530

    Add files via upload

---
+++ scratch_eda.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+st.title("Explanatory Data Analysis")
+st.sidebar.title("Upload Data")
+uploaded_data=st.sidebar.file_uploader("Upload csv file",['csv'])
+tab1,tab2,tab3,tab4=st.tabs(['Data Info','Numeric Features','Categorical Features','Show Data'])
+if uploaded_data is not None:
+    data=pd.read_csv(uploaded_data)
+    with tab1:
+        rows=data.shape[0]
+        cols= data.shape[1]
+        duplicates=data[data.duplicated()]
+        no_duplicates= duplicates.shape[0]
+        no_missing_val= data[data.isna().any(axis=1)].shape[0]
+        st.title("Meta Data")
+        table_markdown= f"""
+            | Description| Value|
+            |-------------|------|
+            | Number of Rows | {rows}|
+            | Number of Columns | {cols}
+            | Number of Duplicate Rows | { no_duplicates}|
+            | Number of missing values | {no_missing_val}|
+            """
+        st.markdown(table_markdown)
+        st.title("Column Information")
+        columns = list(data.columns)
+        table_2 = pd.DataFrame({"Columns":columns,"Types": data.dtypes.to_list()})
+        st.dataframe(table_2,hide_index=True)    
+    with tab2:
+        st.header("Numeric Features to be explored")
+        num_col= data.select_dtypes(include="number").columns.to_list()
+        selected_col=st.selectbox("What column do you want to choose?",num_col)
+        st.header(f"{selected_col} - Statistics")
+        col1= data[selected_col].nunique()
+        col2= data[selected_col].isna().sum()
+        col3=data[selected_col].eq(0).sum()
+        col4=data[selected_col].lt(0).sum()
+        col5= data[selected_col].mean()
+        col6=data[selected_col].median()
+        col7=np.sqrt(data[selected_col].var())
+        col8=data[selected_col].min()
+        col9=data[selected_col].max()
+        dict={"No.of Unique Values":col1,"No.of Rows with Missing Values": col2,"No.of Rows with 0": col3,"No.of Rows with negative Values": col4,"Average Value": col5,"Median": col6,"Min Value": col8,"Max Value": col9,"Sd":col7}
+        info_df=pd.DataFrame(list(dict.items()),columns=["description","Value"])
+        st.dataframe(info_df)
+        st.header("Histogram")
+        plt.hist(data[selected_col])
+        st.pyplot()  
+    with tab3:
+        st.header("Categorical Features Exploring")  
+        cat_fea=data.select_dtypes(include='object').columns.to_list()
+        selected_cat=st.selectbox("Choose a categorical feature",cat_fea)
+        cat_col={}
+        cat_col["No.of unique values"]=data[selected_cat].nunique()
+        cat_col["No.of Rows with missing values"]= data[selected_cat].isna().sum()
+        cat_col["No.of Empty Rows"]=data[selected_cat].eq("").sum()
+        cat_col["No. of Rows with only whitespaces"]=data[selected_cat].str.isspace().sum()
+        cat_col["No.of rows with uppercases"]=data[selected_cat].str.isupper().sum()
+        cat_col["No. of Rows with Alphabets"]=data[selected_cat].str.isalpha().sum()
+        cat_col["No.of Rows with only digits"]=data[selected_cat].str.isdigit().sum()
+        cat_df=pd.DataFrame(list(cat_col.items()),columns=["Description","Values"])
+        st.table(cat_df)
+    with tab4:
+        st.header("Data is as follows:")
+        st.dataframe(data)
